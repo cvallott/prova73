747701c97fa750fd67749cb23db5b1b0315eacd2
cvallott <caterina.vallotto@studenti.unipd.it>
2024-05-15 17:07:18 +0200

    SetUp progetto

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// [CATERINA] [VALLOTTO] [2076434]
+// [VICTORIA] [DURNEA] [2076324]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
